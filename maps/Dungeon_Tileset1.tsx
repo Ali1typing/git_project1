476834dc5f5ad9759ac351242b39042fb032effd
Ali1typing <97065837+Ali1typing@users.noreply.github.com>
2022-03-01 21:01:14 +0300

    Add files via upload

---
+++ maps/Dungeon_Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dungeon_Tileset1" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="16" columns="4">
+ <image source="../../Dungeon_Tileset1.png" width="158" height="160"/>
+</tileset>
